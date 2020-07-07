9a851bbd67c54578a51bd9cbb01596cf2ab3be91
yoloroy <slart_ru@mail.ru>
2020-07-07 16:30:13 +0300

    add spaces

--- app/src/main/java/com/yoloyoj/hse_homework1/MainActivity.kt
+++ app/src/main/java/com/yoloyoj/hse_homework1/MainActivity.kt
@@ -3,15 +3,15 @@ package com.yoloyoj.hse_homework1
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yoloyoj.hse_homework1.projectidea.ProjectIdea
 import com.yoloyoj.hse_homework1.skillitem.SkillItem
 import com.yoloyoj.hse_homework1.userinfo.UserInfo
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,10 +33,6 @@ class MainActivity : AppCompatActivity() {
         )
 
         recycler_view.adapter = adapter
-        recycler_view.adapter!!.notifyDataSetChanged()
-    }
-
-    public fun onClickGotoGithub(view: View) {
-        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/yoloroy")))
+        recycler_view.addItemDecoration(SpaceItemDecoration())
     }
 }
---
+++ app/src/main/java/com/yoloyoj/hse_homework1/SpaceItemDecoration.kt
@@ -0,0 +1,30 @@
+package com.yoloyoj.hse_homework1
+
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+
+class SpaceItemDecoration : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.apply {
+            top = 4.toDp(view)
+            bottom = 4.toDp(view)
+            left = 8.toDp(view)
+            right = 8.toDp(view)
+        }
+    }
+}
+
+fun Number.toDp(view: View) =
+    TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        view.getResources().getDisplayMetrics()
+    ).toInt()
