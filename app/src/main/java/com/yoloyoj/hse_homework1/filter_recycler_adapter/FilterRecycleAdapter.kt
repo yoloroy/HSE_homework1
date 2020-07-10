41209de72ed8101c02a2439ac3a781ce2650db11
yoloroy <slart2108@gmail.com>
2020-07-10 16:53:31 +0300

    Update FilterRecycleAdapter.kt

--- app/src/main/java/com/yoloyoj/hse_homework1/filter_recycler_adapter/FilterRecycleAdapter.kt
+++ app/src/main/java/com/yoloyoj/hse_homework1/filter_recycler_adapter/FilterRecycleAdapter.kt
@@ -27,8 +27,8 @@ class FilterRecycleAdapter(var items: List<FilterItem>) : RecyclerView.Adapter<F
             view.text = filterItem.text
             view.isChecked = filterItem.value
 
-            view.setOnCheckedChangeListener { _, b ->
-                items[position].value = b
+            view.setOnCheckedChangeListener { _, bool ->
+                items[position].value = bool
             }
         }
     }
